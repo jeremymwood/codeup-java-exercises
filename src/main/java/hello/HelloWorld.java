0b39c80b46841dfbc55595cf6c570bdfedd7f72a
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-07 10:32:13 -0600

    1031

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,8 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
         //the program starts executing here
-        System.out.println("Hello world");
+        System.out.print("Hello world");
+        System.out.print("Hello world1");
+        System.out.print("Hello world2");
     }
 }
