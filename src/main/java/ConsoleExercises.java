d6ebbc54a4e571c2856febbc04575730d8205737
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-07 15:59:12 -0600

    1559

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ConsoleExercises {
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.\n", pi);
+
+        Scanner input = new Scanner(System.in);
+
+//        System.out.print("enter a integer: ");
+//        int anInt = input.nextInt();
+//        System.out.println("your integer is: " + anInt);
+
+        System.out.println("enter first word: ");
+        String firstWord = input.next();
+//        System.out.println("first word: " + firstWord);
+
+        System.out.println("enter second word: ");
+        String secondWord = input.next();
+//        System.out.println("second word: " + secondWord);
+
+        System.out.println("enter third word: ");
+        String thirdWord = input.next();
+//        System.out.println("third word: " + thirdWord);
+        System.out.printf("words: %s, %s, %s", firstWord, secondWord, thirdWord);
+    }
+
 }
