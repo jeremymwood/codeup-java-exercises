826699acddb7d1a11a934c9c31e9ffcd862fab46
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-07 10:14:45 -0600

    first commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //the program starts executing here
+        System.out.println("Hello world");
+    }
+}
