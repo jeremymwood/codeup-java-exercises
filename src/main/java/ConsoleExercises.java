58e9b162a7ab54b621d6ac6cce3343e501cc3050
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-07 18:07:08 -0600

    1807

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -11,18 +11,29 @@ public class ConsoleExercises {
 //        int anInt = input.nextInt();
 //        System.out.println("your integer is: " + anInt);
 
-        System.out.println("enter first word: ");
-        String firstWord = input.next();
-//        System.out.println("first word: " + firstWord);
+//        System.out.println("enter first word: ");
+//        String firstWord = input.next();
+////        System.out.println("first word: " + firstWord);
+//
+//        System.out.println("enter second word: ");
+//        String secondWord = input.next();
+////        System.out.println("second word: " + secondWord);
+//
+//        System.out.println("enter third word: ");
+//        String thirdWord = input.next();
+////        System.out.println("third word: " + thirdWord);
+//        System.out.printf("words: %s, %s, %s", firstWord, secondWord, thirdWord);
 
-        System.out.println("enter second word: ");
-        String secondWord = input.next();
-//        System.out.println("second word: " + secondWord);
+        System.out.print("enter room's width: ");
+        int roomWidth = input.nextInt();
+        System.out.print("enter room's length: ");
+        int roomLength = input.nextInt();
+        System.out.println("your room's area is: " + roomWidth * roomLength + " square feet");        System.out.println("your room's perimeter is: " + (2 * roomWidth + 2 * roomLength) + " linear feet");
 
-        System.out.println("enter third word: ");
-        String thirdWord = input.next();
-//        System.out.println("third word: " + thirdWord);
-        System.out.printf("words: %s, %s, %s", firstWord, secondWord, thirdWord);
+        System.out.print("enter room's height: ");
+        int roomHeight = input.nextInt();
+        System.out.println("your room's perimeter is: " + (roomWidth * roomLength * roomHeight) + " cubic feet");
+
     }
 
 }
