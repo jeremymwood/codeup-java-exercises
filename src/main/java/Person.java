c61c0f40ae79c5cc2570c6369c142b9b8beaacdf
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-13 13:40:54 -0600

    1340

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,59 +1,69 @@
 public class Person {
 
     public static int personCounter = 0;
-    private String fName;
-    private String mName;
-    private String lName;
+    private String firstName;
+    private String middleName;
+    private String lastName;
 
-    //constructors
-    public Person() {
-        System.out.println("constructor called");
-        fName = "(first)";
-        mName = "(middle)";
-        lName = "(last)";
+    public Person(String firstName, String middleName, String lastName) {
+        Person.personCounter++;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
     }
 
+    //constructors
+
+    public Person(String firstName) {
+        this("-first-", "-middle-", "-last-");
+        //        Person.personCounter++;
+//        this.firstName = "(first)";
+//        middleName = "(middle)";
+//        lastName = "(last)";
+    }
+
+
     public String sayHello() {
-        return String.format("Hello from %s %s %s. Person counter: %s", fName, mName, lName, personCounter);
+        return String.format("Hello from %s %s %s. \nPerson counter: %s\n", firstName, middleName, lastName, personCounter);
     }
 
     public static void main(String[] args) {
-        Person rick = new Person();
-        rick.fName = "Rick";
-        rick.mName = "Daniel";
-        rick.lName = "Sanchez";
-        System.out.println(rick.sayHello());
-
-        Person sue = new Person();
-        sue.fName = "Sue";
-        sue.lName = "Smith";
-        System.out.println(sue.sayHello());
+//        Person rick = new Person();
+//        rick.firstName = "Rick";
+//        rick.middleName = "Daniel";
+//        rick.lastName = "Sanchez";
+//        System.out.println(rick.sayHello());
+//
+//        Person sue = new Person();
+//        sue.firstName = "Sue";
+//        sue.lastName = "Smith";
+//        System.out.println(sue.sayHello());
     }
 
     //accessors: go at bottom
 
 
-    public String getfName() {
-        return fName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setfName(String fName) {
-        this.fName = fName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getmName() {
-        return mName;
+    public String getMiddleName() {
+        return middleName;
     }
 
-    public void setmName(String mName) {
-        this.mName = mName;
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
     }
 
-    public String getlName() {
-        return lName;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setlName(String lName) {
-        this.lName = lName;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/main/java/oopLec.java
+++ src/main/java/oopLec.java
@@ -1,16 +1,16 @@
 public class oopLec {
     public static void main(String[] args) {
-        Person alex = new Person();
-        Person.personCounter++;
-        alex.setfName("Alex");
-        alex.setmName("M");
-        alex.setlName("S");
-        System.out.println(alex.getfName());
+        Person alex = new Person("Alex", "G", "S");
+//        Person.personCounter++;
+//        alex.setfName("Alex");
+//        alex.setmName("M");
+//        alex.setlName("S");
+        System.out.println(alex.getFirstName());
         System.out.println(alex.sayHello());
 
-        Person sue = new Person();
-        Person.personCounter++;
-        sue.setfName("Sue");
+        Person sue = new Person("Sue");
+//        Person.personCounter++;
+//        sue.setFirstName("Sue");
         System.out.println(sue.sayHello());
     }
 }
