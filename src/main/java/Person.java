aa381488354c94366d16694e9f410d2fe6ad8885
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-13 11:26:36 -0600

    1126

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,20 +1,23 @@
 public class Person {
     public String fName;
+    public String mName;
     public String lName;
 
     public String sayHello() {
-        return String.format("Hello from %s %s", fName, lName);
+        return String.format("Hello from %s %s %s", fName, mName, lName);
     }
 
     public static void main(String[] args) {
         Person rick = new Person();
         rick.fName = "Rick";
+        rick.mName = "Daniel";
         rick.lName = "Sanchez";
         System.out.println(rick.sayHello());
 
         Person sue = new Person();
         sue.fName = "Sue";
-        sue.lName = "Sanchez";
+        sue.mName = "Anne";
+        sue.lName = "Smith";
         System.out.println(sue.sayHello());
     }
 }
--- src/main/java/oopLec.java
+++ src/main/java/oopLec.java
@@ -1,5 +1,10 @@
 public class oopLec {
     public static void main(String[] args) {
-
+        Person alex = new Person();
+        alex.fName = "Alex";
+        alex.mName = "G";
+        alex.lName = "S";
+        System.out.println(alex.fName);
+        System.out.println(alex.sayHello());
     }
 }
