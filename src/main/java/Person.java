b7d6f591d91dc56e3792a4c6b50bfb5c17b554ea
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-13 11:57:50 -0600

    1157

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,10 +1,20 @@
 public class Person {
-    public String fName;
-    public String mName;
-    public String lName;
+
+    public static int personCounter = 0;
+    private String fName;
+    private String mName;
+    private String lName;
+
+    //constructors
+    public Person() {
+        System.out.println("constructor called");
+        fName = "(first)";
+        mName = "(middle)";
+        lName = "(last)";
+    }
 
     public String sayHello() {
-        return String.format("Hello from %s %s %s", fName, mName, lName);
+        return String.format("Hello from %s %s %s. Person counter: %s", fName, mName, lName, personCounter);
     }
 
     public static void main(String[] args) {
@@ -16,8 +26,34 @@ public class Person {
 
         Person sue = new Person();
         sue.fName = "Sue";
-        sue.mName = "Anne";
         sue.lName = "Smith";
         System.out.println(sue.sayHello());
+    }
+
+    //accessors: go at bottom
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
     }
 }
--- src/main/java/oopLec.java
+++ src/main/java/oopLec.java
@@ -1,10 +1,16 @@
 public class oopLec {
     public static void main(String[] args) {
         Person alex = new Person();
-        alex.fName = "Alex";
-        alex.mName = "G";
-        alex.lName = "S";
-        System.out.println(alex.fName);
+        Person.personCounter++;
+        alex.setfName("Alex");
+        alex.setmName("M");
+        alex.setlName("S");
+        System.out.println(alex.getfName());
         System.out.println(alex.sayHello());
+
+        Person sue = new Person();
+        Person.personCounter++;
+        sue.setfName("Sue");
+        System.out.println(sue.sayHello());
     }
 }
