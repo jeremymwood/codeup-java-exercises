647f4d881f91d69071af8aac343b6387565b8732
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-14 14:36:29 -0600

    1436

--- src/main/java/ServerNameGenerator.java
+++ src/main/java/ServerNameGenerator.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+
 public class ServerNameGenerator {
 
+
+    public static void main(String[] args) {
     String[] adjectives = {"odd", "comprehensive", "shrill", "simplistic", "healthy", "obese", "square", "kaput", "fluffy", "squealing"};
 
     String[] nouns = {"loss", "leadership", "church", "member", "procedure", "confusion", "magazine", "childhood", "elevator", "protection"};
 
+    int lowerBound = 0;
+    int upperBound = adjectives.length;
+    int range = upperBound - lowerBound;
+    int random = (int)(Math.random() * range) + lowerBound;
+    System.out.printf("Here is your server name:\n%s-%s", adjectives[random], nouns[random]);
+    }
 
 
 }
