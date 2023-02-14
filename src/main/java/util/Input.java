b6ee07fad2e3a02f7d3df640172a94a5f7943b72
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-13 19:34:46 -0600

    1933

--- src/main/java/util/Input.java
+++ src/main/java/util/Input.java
@@ -26,4 +26,37 @@ public class Input {
             return false;
         }
     }
+
+    public static int getInt(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter number between %s and %s: ", min, max);
+        int userNum = scanner.nextInt();
+        if (userNum >= min && userNum <= max) {
+            if (userNum >= min && userNum <= max) {
+            System.out.printf("huzzah! %s is in range:)", userNum);
+                return userNum;
+            }
+            System.out.printf("%s is out of range, try again:/\n", userNum);
+            return getInt(min, max);
+        }
+        System.out.println("all done");
+        return userNum;
+
+
+//            int myNumber = (int) (Math.random() * 100) + 1;
+//            int userInput;
+//
+//            do {
+//                System.out.println("Choose a number between 1 and 100.");
+//                userInput = getInt(1, 100);
+//                if (userInput > myNumber) {
+//                    System.out.println("Lower");
+//                } else if ( userInput < myNumber) {
+//                    System.out.println("Higher");
+//                } else {
+//                    System.out.println("Nice one Rainman!");
+//                }
+//            } while (userInput != myNumber);
+//            System.out.println("Game over.");
+    }
 }
--- src/main/java/util/InputTest.java
+++ src/main/java/util/InputTest.java
@@ -1,23 +1,10 @@
 package util;
 
-import java.util.Scanner;
-
 public class InputTest {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
 //        Input.getString();
-        Input.yesNo();
-
-//        System.out.print("enter a string:");
-//        String myString = Input.scanner.next();
-//        Input.getString(myString);
-//        System.out.printf("Your string: %s", myString);
-
-//        System.out.print("enter a boolean:");
-//        Boolean myBool = scanner.next();
-//        System.out.printf("Your boolean: %s", myBool);
-
-
+//        Input.yesNo();
+        Input.getInt(1, 10);
 
     }
 }
