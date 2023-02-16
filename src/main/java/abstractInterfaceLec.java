218262e22fecba90505172a06ab3797589c0ed48
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-16 10:10:12 -0600

    1009-end-lec

---
+++ src/main/java/abstractInterfaceLec.java
@@ -0,0 +1,29 @@
+public class abstractInterfaceLec {
+
+
+//    ***properties***
+
+//    ***constructor***
+
+//    ***getters and setters***
+
+//    ***abstract method***
+
+
+//    abstract classes_____________
+
+//    can have multiple abstract classes for npc's, cryptids, etc...
+
+//    object -> abstract -> unique subclasses
+
+
+//    interfaces__________
+
+//    behavioral, ex. jumpable, vulnerable, etc...
+
+//    define actions, no getters/setters, no properties, etc...
+
+//    class implements behavior of interface
+
+
+}
