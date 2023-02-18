7e46fb0d67e661a7f04be86466f4f4dc6bc5f7f6
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-18 16:41:02 -0600

    1640-grocery-list-recipe

---
+++ src/main/java/grocerylist/Groceries.java
@@ -0,0 +1,24 @@
+package grocerylist;
+
+public class Groceries {
+//    grocery list command line application
+//
+//    use  input class
+//
+//    prompt to create a grocery list
+//      no: close application
+//
+//      yes: prompt for new item
+//          yes: three prompts:
+//              1) select category
+//                  produce
+//                  dairy
+//                  dry goods
+//                  frozen
+//              2) Enter item name
+//              3) Enter item quantity
+//          finalize or add another item?
+//
+//    print list a-z by category with quantity
+
+}
