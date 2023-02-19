5b57dc80fc62be200083ac1a92a93d71fc91b1aa
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-19 14:26:12 -0600

    1426

--- src/main/java/grocerylist/Groceries.java
+++ src/main/java/grocerylist/Groceries.java
@@ -1,76 +1,87 @@
 package grocerylist;
 
+import grades.Student;
 import util.Input;
 
-import java.awt.event.ItemEvent;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
 
 public class Groceries {
-//    grocery list command line application
-//
-//    use  input class
     private static Input input = new Input();
-
     private static HashMap<String, Item> items;
-
     public static void main(String[] args) {
         Input input = new Input();
-
         items = new HashMap<>();
-
 //        Item apples = new Item("apples");
 //        apples.addQuantity(4);
 //        System.out.println(apples.TotalQuantity);
 //        apples.addQuantity(4);
-//        System.out.println(apples.getTotalQuantity());
 //        System.out.println(apples);
-//
 //        apples.addCategory("produce");
 //        System.out.println(apples);
-//
 //        items.put("apples", apples);
-
+         System.out.print("Would you like to make a grocery list? (Y/N)");
          do {
-            printMenu();
             boolean choice = Input.yesNo();
-            if(!choice) {
-                break;
+            if(choice) {
+                categoryMenu();
             }
+             break;
         } while(true);
-
         System.out.println("Bye");
 
     }
-
-    //
-//    prompt to create a grocery list
-//      no: close application
-//
-//      yes: prompt for new item
-//          yes: three prompts:
-//              1) select category
-//                  produce
-//                  dairy
-//                  dry goods
-//                  frozen
 //              2) Enter item name
 //              3) Enter item quantity
 //          finalize or add another item?
-//
 //    print list a-z by category with quantity
+        private static void categoryMenu() {
+            System.out.print("""
+                    Select a category to add an item to:
 
-    private static void printMenu() {
-        System.out.print("""
-                Would you like to make a grocery list?
+                    1: Produce
+                    2: Dairy
+                    3: Dry goods
+                    4: Frozen
+                    5: Exit
+                    """);
 
-                0: Exit
-                1: Print student info
-                2: View all the grades
-                3: Overall class average
-                4: Print CSV report
+            do {
+                int choice = input.getInt(1, 5, "");
+                doChoice(choice);
+                if(choice == 5) {
+                    break;
+                }
+            } while(true);
 
-                Enter your choice:
-                """);
+        }
+
+    private static void doChoice(int choice) {
+        switch (choice) {
+            case 1:
+                addProduce();
+                break;
+            case 2:
+//                addDairy();
+                break;
+            case 3:
+//                addDryGoods();
+                break;
+            case 4:
+//                AddFrozen();
+                break;
+            case 5:
+                break;
+        }
     }
+
+    private static void addProduce() {
+        Set<String> keys = items.keySet();
+
+        for (String key : keys) {
+            Item anItem = items.get(key);
+            System.out.print("Enter fruit or vegetable:");
+        }
+    }
+
 }
